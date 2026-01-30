e54a6ba792226940ebb852c737b91b0720f268d4
Prashant Bhadu <prashantbhadu97@gmail.com>
2026-01-30 22:25:41 +0530

    store and carts functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
+from .forms import RegistrationForm, UserLoginForm
+from .models import Account
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            user.phone_number = phone_number
+            user.save()
+            messages.success(request, 'Registration successful.')
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+    
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/register.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = auth.authenticate(email=email, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                messages.success(request, 'You are now logged in.')
+                return redirect('home')
+            else:
+                messages.error(request, 'Invalid login credentials')
+    else:
+        form = UserLoginForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+@login_required(login_url='login')
+def logout(request):
+    auth.logout(request)
+    messages.success(request, 'You are logged out.')
+    return redirect('login')
--- greatkart/settings.py
+++ greatkart/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "category",
     "accounts",
     "store",
+    "carts",
 ]
 
 MIDDLEWARE = [
@@ -65,6 +66,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "category.context_processors.menu_links",
+                "carts.context_processors.counter",
             ],
         },
     },
@@ -130,3 +132,8 @@ STATICFILES_DIRS = [
 MEDIA_URL ='/media/'
 MEDIA_ROOT = BASE_DIR/'media'
 
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
